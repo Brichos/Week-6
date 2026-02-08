605707a94f67781b65158ca428b9a9756e1755a9
Brichos <nicholasak@uia.no>
2026-02-08 13:56:43 +0100

    Added pipe function

--- week6.mjs
+++ week6.mjs
@@ -1,4 +1,13 @@
+//Array function
+
 export const intersection = (arr1, arr2) => {
   const set2 = new Set(arr2);
   return [...new Set(arr1.filter(el => set2.has(el)))];
+};
+
+//Pipe function
+export const pipe = (...fns) => {
+  return (initialValue) => {
+    return fns.reduce((value, fn) => fn(value), initialValue);
+  };
 };
