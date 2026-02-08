e7b83f3d1ce73c2740f58efc632b5e03e0cc1fa0
Brichos <nicholasak@uia.no>
2026-02-08 13:18:51 +0100

    added basic intersection test

--- week6.test.mjs
+++ week6.test.mjs
@@ -1,2 +1,8 @@
 import {strick as assert } from 'assert';
 import { intersection } from './week6.mjs';
+
+console.log('TEsting Array Intersection')
+
+//Basic intersection
+assert.deepEqual(intersection([1, 2, 3,], [2, 3, 4]), [2, 3]);
+console.log('Basic intersection');
